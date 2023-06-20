e88d43dae6c28aa00b6fd9ceab22fee5936d2e1a
François Beausoleil <francois@teksol.info>
2023-06-20 15:39:55 -0400

    Replace deprecated usage of RSpec #should with #expect syntax

--- spec/jaccard_spec.rb
+++ spec/jaccard_spec.rb
@@ -11,49 +11,49 @@ describe Jaccard do
   end
 
   describe "#coefficient" do
-    it "should calculate the coefficient on Array instances" do
-      Jaccard.coefficient(a, b).should == 0.25
-      Jaccard.coefficient(a, c).should == 0.0
-      Jaccard.coefficient(b, c).should == (2.0/3.0)
+    it "calculates the coefficient on Array instances" do
+      expect(Jaccard.coefficient(a, b)).to eq(0.25)
+      expect(Jaccard.coefficient(a, c)).to eq(0.0)
+      expect(Jaccard.coefficient(b, c)).to eq(2.0 / 3.0)
     end
 
-    it "should calculate the coefficient on Set instances" do
-      Jaccard.coefficient(a.to_set, b.to_set).should == 0.25
-      Jaccard.coefficient(a.to_set, c.to_set).should == 0.0
-      Jaccard.coefficient(b.to_set, c.to_set).should == (2.0/3.0)
+    it "calculates the coefficient on Set instances" do
+      expect(Jaccard.coefficient(a.to_set, b.to_set)).to eq(0.25)
+      expect(Jaccard.coefficient(a.to_set, c.to_set)).to eq(0.0)
+      expect(Jaccard.coefficient(b.to_set, c.to_set)).to eq(2.0 / 3.0)
     end
 
     it do
       obj_without_intersection_method = ""
-      lambda { Jaccard.coefficient(obj_without_intersection_method, []) }.should raise_error(ArgumentError)
+      expect { Jaccard.coefficient(obj_without_intersection_method, []) }.to raise_error(ArgumentError)
     end
 
     it do
       obj_without_union_method = Object.new
       def obj_without_union_method.&(other); self; end
 
-      lambda { Jaccard.coefficient(obj_without_union_method, []) }.should raise_error(ArgumentError)
+      expect { Jaccard.coefficient(obj_without_union_method, []) }.to raise_error(ArgumentError)
     end
   end
 
   describe "#distance" do
-    it "should calculate the distance as the inverse of the coefficient" do
-      Jaccard.distance(a, b).should == (1.0 - Jaccard.coefficient(a, b))
-      Jaccard.distance(a, c).should == (1.0 - Jaccard.coefficient(a, c))
-      Jaccard.distance(b, c).should == (1.0 - Jaccard.coefficient(b, c))
+    it "calculates the distance as the inverse of the coefficient" do
+      expect(Jaccard.distance(a, b)).to eq(1.0 - Jaccard.coefficient(a, b))
+      expect(Jaccard.distance(a, c)).to eq(1.0 - Jaccard.coefficient(a, c))
+      expect(Jaccard.distance(b, c)).to eq(1.0 - Jaccard.coefficient(b, c))
     end
   end
 
   describe "#closest_to" do
-    it "should minimize the distance" do
-      Jaccard.closest_to(a, [b, c]).should == b
-      Jaccard.closest_to(b, [a, c]).should == c
+    it "minimizes the distance" do
+      expect(Jaccard.closest_to(a, [b, c])).to be_equal( b)
+      expect(Jaccard.closest_to(b, [a, c])).to be_equal( c)
     end
   end
 
   describe "#best_match" do
-    it "should find the best match given a set of entries" do
-      Jaccard.best_match([a, b, c]).should == [b, c]
+    it "finds the best match given a set of entries" do
+      expect(Jaccard.best_match([a, b, c])).to eq([b, c])
     end
   end
 end
